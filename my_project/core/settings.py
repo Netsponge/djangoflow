b0c26eec04e3e87b0e3a95504210201d26a7b9ac
jean-baptiste <jeanbaptiste.marrec@gmail.com>
2024-11-21 16:11:01 +0100

    delete old file tree

--- my_project/core/settings.py
+++ my_project/core/settings.py
@@ -1,4 +1,4 @@
-
+import os
 """
 Django settings for core project.
 
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g^dk!15o7ad$v%dmfqfq_4^)yby6#arhi#jz($%d*zqd3+2mp+'
+SECRET_KEY = 'django-insecure-#jct4_c1po_t0fxj(l*^h-kl!5p_h^!%j!avfxc$ch9z466=60'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts',
 ]
 
 MIDDLEWARE = [
@@ -127,4 +126,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
- 
